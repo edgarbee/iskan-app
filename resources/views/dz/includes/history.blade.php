0d74943ba8c7e4458ef1f4d7ecb927d8b700a09b
edgarbee <57279210+edgarbee@users.noreply.github.com>
2023-04-05 18:13:11 +0300

    add history/karta_sber/validate email users

--- resources/views/dz/includes/history.blade.php
+++ resources/views/dz/includes/history.blade.php
@@ -6,7 +6,7 @@
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-                @if(!empty(\Auth::user()->getHistory))
+                @if(!\Auth::user()->getHistory->isEmpty())
                     @foreach(\Auth::user()->getHistory->reverse() as $history)
                         <p><a href="{{ route('historyDz', ['dz_id' => $history->dz_id]) }}">{{ $history->title }} (Дата: {{ \Carbon\Carbon::parse($history->created_at)->format('d.m.Y H:i')}})</a></p>
                     @endforeach
