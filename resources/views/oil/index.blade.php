670033e18a6f0c67fa2d2a6a945bc8e9ebf38436
edgarbee <57279210+edgarbee@users.noreply.github.com>
2023-04-14 08:53:29 +0300

    fix form oil

--- resources/views/oil/index.blade.php
+++ resources/views/oil/index.blade.php
@@ -44,6 +44,7 @@
                         </div>
                     </div>
                 @endif
+
                 <div class="col-md-12 mt-2">
                     <div class="row">
                         <div class="col-md-12 mb-2" style="font-size: 12px; font-weight: 700">
